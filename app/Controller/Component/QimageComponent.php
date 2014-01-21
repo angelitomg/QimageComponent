4a6d3ceffa0d72d9a2d991b1d4fa81c0928dec7a
Angelito M. Goulart <angelito@bsd.com.br>
2014-01-21 12:05:36 -0200

    Update QimageComponent.php
    Add crop function

--- app/Controller/Component/QimageComponent.php
+++ app/Controller/Component/QimageComponent.php
@@ -4,53 +4,60 @@
 	
 	
 	/**
-	*
-	* CakePHP (version 2) component for upload, resize and
-	* add watermark to images.
-	*
-	* @author: 
-	* Angelito M. Goulart
-	* <angelito@bsd.com.br>
-	* www.angelitomg.com
-	* github.com/angelitomg
-	*
-	*/
+	 *
+	 * CakePHP (version 2) component to upload, resize, crop and
+	 * add watermark to images.
+	 *
+	 * @author Angelito M. Goulart <ange.sap@hotmail.com>
+	 *
+	 */
 	class QimageComponent extends Component{
 		
 		/**
-		* Watermark image file (must be png)
-		* @var string
-		*/
+		 * Watermark image file (must be png)
+		 *
+		 * @var string
+		 */
 		public $watermarkImage;		
+
+
+		/**
+		 * Jpeg image quality (0 - 100)
+		 *
+		 * @var int
+		 */
+		public $jpgQuality;
 
 		
 		/**
-		* Property that will contain execution errors
-		* @var array
-		*/
-		public $errors;
+		 * Property that will contain execution errors
+		 *
+		 * @var array
+		 */
+		private $errors;
 	
 		
 		/**
-		* Initialize method. Initialize class properties.
-		*/
-		public function initialize(){
+		 * Initialize method. Initialize class properties.
+		 *
+		 * @param Controller $controller
+		 */
+		public function initialize(Controller $controller) {
 			$this->watermarkImage = "img" . DIRECTORY_SEPARATOR . "watermark.png";
+			$this->jpgQuality = 100;
 			$this->errors = array();
 		}
 	
 		
 		/**
-		*
-		* Copy an uploaded image to the destination path
-		*
-		* $data['file'] 	-> array with image data (found in $_FILES)
-		* $data['path'] 	-> destination path
-		*
-		* @param array $data
-		* @return mixed
-		*
-		*/
+		 * Copy an uploaded image to the destination path
+		 *
+		 * $data['file'] 	-> array with image data (found in $_FILES)
+		 * $data['path'] 	-> destination path
+		 *
+		 * @param array $data
+		 * @return mixed
+		 */
 		public function copy($data){
 			
 			// Verify file and path
@@ -64,8 +71,8 @@
 				return false;
 			}
 			
-			if (!$this->verifyMime($data['file']['name'])){
-				$this->errors[] = 'The file must be an image!';
+			if (!$this->_verifyMime($data['file']['name'])){
+				$this->errors[] = 'The file must be an jpg, gif or png image!';
 				return false;
 			}
 			
@@ -74,7 +81,7 @@
 			$ext = end($filename_array);
 			$ext = strtolower($ext);
 			$name = uniqid() . date('dmYHis') . '.' . $ext;
-			$complete_path = $data['path'] . $name;
+			$complete_path = $data['path'] . DIRECTORY_SEPARATOR . $name;
 			
 			if (!move_uploaded_file($data['file']['tmp_name'], $data['path'] . $name)){
 				$this->errors[] = 'Error while upload the image!';
@@ -89,16 +96,14 @@
 		
 		
 		/**
-		*
-		* Adds a watermark on footer of a image.
-		* The watermark image file must be informed in public $watermarkImage.
-		*
-		* $data['file'] -> image path
-		*
-		* @param array $data
-		* @return bool
-		*
-		*/
+		 * Adds a watermark on footer of an image.
+		 * The watermark image file must be informed in public $watermarkImage.
+		 *
+		 * $data['file'] -> image path
+		 *
+		 * @param array $data
+		 * @return bool
+		 */
 		public function watermark($data){
 			
 			// Verify files
@@ -112,13 +117,21 @@
 				return false;
 			}
 			
-			if(!$this->verifyMime($data['file'])){
+			if(!$this->_verifyMime($data['file'])){
 				$this->errors[] = 'Invalid file type!';
 				return false;
 			}
 			
-			// Get watermark image info
+			// Get image info
 			$img = getimagesize($data['file']);
+
+			// Get watermark image info
+			$wimg = getimagesize($this->watermarkImage);
+			if ($wimg['mime'] !== 'image/png') {
+				$this->errors[] = 'Watermark image must be png!';
+				return false;
+			}
+
 			$watermark = imagecreatefrompng($this->watermarkImage);
 			$watermark_width = imagesx($watermark);
 			$watermark_height = imagesy($watermark);
@@ -127,17 +140,9 @@
 			$margin_right = $img[0] - $watermark_width - 15;
 			$margin_bottom = $img[1] - $watermark_height - 15;
 			
-			// Define the function to be used for each image type
-			if ($img['mime'] == 'image/jpeg' || $img['mime'] == 'image/pjpeg'){
-				$createFunction = 'imagecreatefromjpeg';
-				$finalizeFunction = 'imagejpeg';
-			} elseif ($img['mime'] == 'image/gif') {
-				$createFunction = 'imagecreatefromgif';
-				$finalizeFunction = 'imagegif';
-			} elseif ($img['mime'] == 'image/png') {
-				$createFunction = 'imagecreatefrompng';
-				$finalizeFunction = 'imagepng';
-			} else {
+			$createFunction = $this->_getCreateFunction($img['mime']);
+			$finishFunction = $this->_getFinishFunction($img['mime']);
+			if (false === $createFunction || false === $finishFunction) {
 				$this->errors[] = 'Invalid file type!';
 				return false;
 			}
@@ -148,9 +153,9 @@
 			
 			// Replace the original image with the new image with watermark
 			if ($img['mime'] == 'image/jpeg' || $img['mime'] == 'image/pjpeg'){
-				$finalizeFunction($image, $data['file'], 100);
+				$finishFunction($image, $data['file'], (int) $this->jpgQuality);
 			} else {
-				$finalizeFunction($image, $data['file']);
+				$finishFunction($image, $data['file']);
 			}
 			
 			return true;
@@ -159,25 +164,23 @@
 		
 		
 		/**
-		*
-		* Method responsible for resize a image. Return false on error.
-		*
-		* $data['file']   		-> complete path of original image file
-		* $data['width']  		-> new width
-		* $data['height'] 		-> new height
-		* $data['output'] 		-> output path where resized image will be saved
-		* $data['proportional'] -> (true or false). If true, the image will be resized 
-		* only if its dimensions are larger than the values reported in width and height 
-		* parameters. Default: true.
-		*
-		* If only the width or height is given, the function will automatically calculate 
-		* whether the image is horizontal or vertical and will automatically apply the informed 
-		* size in the correct property (width or height).
-		*
-		* @param array $data
-		* @return bool
-		*
-		*/
+		 * Method responsible for resize an image. Return false on error.
+		 *
+		 * $data['file']   		-> complete path of original image file
+		 * $data['width']  		-> new width
+		 * $data['height'] 		-> new height
+		 * $data['output'] 		-> output path where resized image will be saved
+		 * $data['proportional'] -> (true or false). If true, the image will be resized 
+		 * only if its dimensions are larger than the values reported in width and height 
+		 * parameters. Default: true.
+		 *
+		 * If only the width or height is given, the function will automatically calculate 
+		 * whether the image is horizontal or vertical and will automatically apply the informed 
+		 * size in the correct property (width or height).
+		 *
+		 * @param array $data
+		 * @return bool
+		 */
 		public function resize($data){
 			
 			// Verify parameters
@@ -205,14 +208,12 @@
 				$this->errors[] = 'Invalid file!';
 				return false;
 			}
-			
-			// Verify mime type
-			if(!$this->verifyMime($data['file'])){
+		
+			if(!$this->_verifyMime($data['file'])){
 				$this->errors[] = 'Invalid file type!';
 				return false;
 			}
 				
-			// Verify if thumb must be proportional
 			if (!isset($data['proportional']))
 				$data['proportional'] = true;	
 			
@@ -259,42 +260,108 @@
 			$thumb = imagecreatetruecolor($width, $height);
 			
 			// Add transparency if image is png
-			if ($mime == 'image/png'){
+			if ($mime == 'image/png') {
 				imagealphablending($thumb, false);
 				imagesavealpha($thumb,true);
 				$transparent = imagecolorallocatealpha($thumb, 255, 255, 255, 127);
 				imagefilledrectangle($thumb, 0, 0, $width, $height, $transparent);
 			} 
 			
-			// Finalize the image
+			// Finish image
 			imagecopyresampled($thumb, $source, 0, 0, 0, 0, $width, $height, $original_width, $original_height);
-			
-			// Verify type of the image
-			if ($mime == 'image/jpeg' || $mime == 'image/pjpeg'){
-				imagejpeg($thumb, $output, 100);
-			} elseif ($mime == 'image/gif') {
-				imagegif($thumb, $output);
-			} elseif ($mime == 'image/png') {
-				imagepng($thumb, $output);
-			} else {
+			$finishFunction = $this->_getFinishFunction($mime); 
+						
+			if (false === $finishFunction) { 
 				$this->errors[] = 'Invalid file type.';
 				return false;	
-			}
-			
+			} elseif ($mime == 'image/jpeg' || $mime == 'image/pjpeg') {
+				$finishFunction($thumb, $output, (int) $this->jpgQuality);
+			} else {
+				$finishFunction($thumb, $output);
+			} 
+
 			return true;
 			
 		}
-		
-		
+
+
 		/**
-		* 
-		* Method responsible for verify the mime-type of a file
-		*
-		* @param string $file
-		* @return bool
-		*
-		*/
-		private function verifyMime($file){
+		 * Method to crop an image
+		 * 
+		 * $data['file']   		-> complete path of original image file
+		 * $data['w']	  		-> width of crop area
+		 * $data['h'] 			-> height of crop area
+		 * $data['x']			-> x coordinate of source point
+		 * $data['y']			-> y coordinate of source point
+		 * $data['output'] 		-> output path where cropped image will be saved
+		 *
+		 * @param array $data
+		 * @return bool
+		 */
+		public function crop($data = array()){
+
+			// Validates params
+			if (!isset($data['file']) ||
+				!isset($data['w']) ||
+				!isset($data['h']) ||
+				!isset($data['x']) ||
+				!isset($data['y']) ||
+				!isset($data['output'])) {
+				$this->errors[] = 'Params missing!';
+			}
+			
+			if (!is_file($data['file'])) {
+				$this->errors[] = 'Invalid image!';
+				return false;
+			}
+
+			if (!is_dir($data['output']) || !is_writable($data['output'])) {
+				$this->errors[] = 'Output dir is not a dir or not writeable!';
+				return false;
+			}
+
+			// Output path
+			$path = $data['output'] . DIRECTORY_SEPARATOR . basename($data['file']);
+
+			// Get image data
+			$img = getimagesize($data['file']);
+
+			$createFunction = $this->_getCreateFunction($img['mime']);
+			$finishFunction = $this->_getFinishFunction($img['mime']);
+
+			// Create source and destination image
+			$src_img = $createFunction($data['file']);
+			$dst_img = imagecreatetruecolor($data['w'], $data['h']);
+
+			// Crop image
+			imagecopyresampled($dst_img, 
+							   $src_img, 
+							   0, 
+							   0, 
+							   (int) $data['x'], 
+							   (int) $data['y'],
+	        				   (int) $data['w'],
+	        				   (int) $data['h'],
+	        				   (int) $data['w'], 
+	        				   (int) $data['h']);
+
+			// Finish image
+			if ($img['mime'] == 'image/jpeg' || $img['mime'] == 'image/pjpeg'){
+				$finishFunction($dst_img, $path, (int) $this->jpgQuality);
+			} else {
+				$finishFunction($dst_img, $path);
+			}
+
+		}
+		
+		
+		/** 
+		 * Verifies the mime-type of a file
+		 *
+		 * @param string $file
+		 * @return bool
+		 */
+		private function _verifyMime($file){
 			
 			$filename_array = explode('.',$file);
 
@@ -311,7 +378,57 @@
 			}
 			
 		}
-		
+
+
+		/**
+		 * Method to get the specific function to create an image
+		 * 
+		 * @param string $mime
+		 * @return string
+		 */
+		private function _getCreateFunction($mime){
+			if ($mime == 'image/jpeg' || $mime == 'image/pjpeg'){
+	            return 'imagecreatefromjpeg';
+		    } elseif ($mime == 'image/gif') {
+		        return 'imagecreatefromgif';
+		    } elseif ($mime == 'image/png') {
+		        return 'imagecreatefrompng';
+		    } else {
+		        $this->errors[] = 'Invalid file type!';
+		        return false;
+		    }
+		}
+
+
+		/**
+		 * Method to get the specific function to finish an image
+		 *
+		 * @param string $mime
+		 * @return string
+		 */
+		private function _getFinishFunction($mime) {
+			if ($mime == 'image/jpeg' || $mime == 'image/pjpeg'){
+				return 'imagejpeg';
+			} elseif ($mime == 'image/gif') {
+				return 'imagegif';
+			} elseif ($mime == 'image/png') {
+				return 'imagepng';
+			} else {
+				$this->errors[] = 'Invalid file type.';
+				return false;	
+			}
+		}
+
+
+		/**
+		 * Get errors
+		 *
+		 * @return array
+		 */
+		public function getErrors() {
+			return $this->errors;
+		}
+
 		
 	}
 	
